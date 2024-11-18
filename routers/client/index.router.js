8c72ce19c6b01460659bd1ff8c6d1af325d741e3
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2024-11-19 01:14:05 +0700

    chat

--- routers/client/index.router.js
+++ routers/client/index.router.js
@@ -1,8 +1,11 @@
 const productRouters = require("./products.router");
 const homeRouters = require("./home.router");
+const chatRouters = require("./chat.router");
 
 module.exports = (app) => {
   app.use("/", homeRouters);
 
   app.use("/products", productRouters);
+
+  app.use("/chat", chatRouters);
 };
